6b6a16b78c47da34d608e35b8a0ff29111f93454
wmartine58 <wellinmart58@gmail.com>
2018-04-03 21:48:15 -0500

    Actualizar datos en aprendizaje
    Subiendo clases nuevas de como hacer formularios

---
+++ Aprendizaje DAW/Html5/Introduccion_javascript/Tips para javascript.js
@@ -0,0 +1,28 @@
+/*Definir variables*/
+var a = document.getElementsByTagName("nombre_etiqueta")[i];
+var b = document.getElementsByClassName("nombre_clase")[i];
+var c = document.getElementById("nombre_id")
+
+/*definir funciones*/
+function nombre_de_la_funcion() {
+	/*Cuerpo de la función*/
+}
+
+/*Como añadir eventos*/
+a.addEvenListener("mouseover", funcion_que_se_dispara(), false");
+				  
+/*Eventos de un escuchador*/
+/*mouseover */
+/*mouseout*/
+/*click*/
+/*load*/
+				  
+/*Propiedades del objeto video*/
+/*duration*/
+/*currentTime*/
+/*paused*/
+/*ended*/
+
+/*Métodos del objeto video*/
+/*play*/
+/*paused*/
